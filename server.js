37ac6fee90eae8d5de367e6373a96c0840adf156
Partially Sorted <partially.sorted@gmail.com>
2021-11-17 18:18:23 -0600

    #

--- server.js
+++ server.js
@@ -34,22 +34,25 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 
-app.use(function (req, res, next) {
-    console.log('middleware');
-    req.testing = 'testing';
-    return next();
-});
+//app.use(function (req, res, next) {
+//    console.log('middleware');
+//    req.testing = 'testing';
+//    return next();
+//});
 
-app.get('/', function(req, res, next){
-    console.log('get route', req.testing);
-    res.end();
-});
+//app.get('/', function(req, res, next){
+//    console.log('get route', req.testing);
+//    res.end();
+//});
 
 app.ws('/ws', function(ws, req) {
     ws.on('message', function(msg) {
-        console.log(msg);
+        console.log("MESSAGE:", msg);
+        msg = JSON.parse(msg);
+        if (msg.op == 'ping') {
+            ws.send(JSON.stringify({ op: "pong", timestamp: new Date() }));
+        }
     });
-    console.log('socket', req.testing);
 });
 
 app.listen(8080);
